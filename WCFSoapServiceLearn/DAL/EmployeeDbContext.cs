0513523f16338bb0b5fd9e26aea7fd46c958fa61
numanakoluk <numanakoluk01@gmail.com>
2022-05-02 13:55:05 +0300

    Model Class and DAL

---
+++ WCFSoapServiceLearn/DAL/EmployeeDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using WCFSoapServiceLearn.Model;
+
+namespace WCFSoapServiceLearn.DAL
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext() : base("EmployeeDbContext")
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    public class EmployeeDbInitilaizer: DropCreateDatabaseIfModelChanges<EmployeeDbContext>
+    {
+        protected override void Seed(EmployeeDbContext context)
+        {
+            context.Employees.Add(new Employee() {
+            Name="Numan",
+            Adress ="Şişli",
+           
+            
+
+            });
+            context.SaveChanges();
+        }
+    }
+}
