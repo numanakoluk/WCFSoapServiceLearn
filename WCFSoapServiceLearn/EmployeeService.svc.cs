27eebc40755ffef9e1b781109e94dc69e9047b6e
numanakoluk <numanakoluk01@gmail.com>
2022-05-02 14:31:18 +0300

    Add WCF Service

---
+++ WCFSoapServiceLearn/EmployeeService.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WCFSoapServiceLearn.Model;
+
+namespace WCFSoapServiceLearn
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select EmployeeService.svc or EmployeeService.svc.cs at the Solution Explorer and start debugging.
+    public class EmployeeService : IEmployeeService
+    {
+        public void Add(Employee e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Employee e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WCFSoapServiceLearn/IEmployeeService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WCFSoapServiceLearn.Model;
+
+namespace WCFSoapServiceLearn
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmployeeService" in both code and config file together.
+    [ServiceContract]
+    public interface IEmployeeService
+    {
+        [OperationContract]
+
+        List<Employee> GetAllEmployees();
+
+        [OperationContract]
+        Employee Get(int id);
+
+        [OperationContract]
+        void Add(Employee e);
+
+        [OperationContract]
+        void Edit(Employee e);
+
+        [OperationContract]
+        void Remove(int id);
+
+    }
+}
